2ac8700b41139df7c0d913d099a1e0f514f784b3
Leah Chen <leeewchen@gmail.com>
2022-08-04 11:49:04 -0700

    map changes

--- client/src/Pages/ChatPage.js
+++ client/src/Pages/ChatPage.js
@@ -3,58 +3,20 @@ import SecondMapBox from '../Components/SecondMapBox'
 import Stack from '@mui/material/Stack'
 
 function ChatPage({ mentorsArray }) {
-  const [current, setCurrent] = useState(0)
-  const length = mentorsArray.length;
-  const nextImage = () => {
-    setCurrent(current === length - 1 ? 0 : current + 1)
-  }
-
-  const previousImage = () => {
-    setCurrent(current === 0 ? length - 1 : current - 1)
-  }
-
-  if (!Array.isArray(mentorsArray) || mentorsArray.length <= 0) {
-    return null;
-  }
 
   return (
-    <body className="chatpage">
-    <Stack direction="column" justifyContent="center" alignItems="center"
-    sx={{marginBottom: '-10px', marginTop: '10px'}}>
-    
-    <img
-        className="second-mapbox-quote"
-        src="https://cdn-images-1.medium.com/max/1600/1*GoiGM1iQZzdB5LbCR1u7eA.png" />
-
-       <section className="slider2">
-  
-        <img
-          className='mentorcard-left-arrow'
-          src="https://cdn-images-1.medium.com/max/800/1*UC5JK7wWPzUW6DFov8WrEA.png"
-          onClick={previousImage}
-        />
-
-        {mentorsArray.map((mentor, index) => {
-
-          return (
-            <div className={index === current ? 'mentor active' : 'mentor'} key={index}>
-              {index === current && (<SecondMapBox
-                key={mentor.id}
-                mentor={mentor}
-              />)}
-
-            </div>
-          )
-        })}
-        < img
-          className="mentorcard-right-arrow"
-          src="https://cdn-images-1.medium.com/max/800/1*YQEIDVTv2oWP2d_USO-P9Q.png"
-          onClick={nextImage} />
-    </section>
-    </Stack>
-    </body>
+    <h1> Chat Page TBD ^____^ </h1>
   );
 
-}
+  }
 
 export default ChatPage
+
+{/* // return (
+      
+//   <div>
+//     <Mapbox
+//       mentorsArray= {mentorsArray} />
+//  </div>
+// );
+// } */}
--- client/src/Pages/MapPage.js
+++ client/src/Pages/MapPage.js
@@ -1,16 +1,61 @@
-import React , {useState} from 'react'
 import Mapbox from '../Components/Mapbox';
+import React, { useState } from 'react'
+import SecondMapBox from '../Components/SecondMapBox'
+import Stack from '@mui/material/Stack'
 
 function MapPage({mentorsArray}) {
-  // const {id, name, background, profession, city, profile_url, longitude, latitude} = mentors;
-  
-    return (
-      
-      <div>
-        <Mapbox
-          mentorsArray= {mentorsArray} />
-     </div>
-    );
+  const [current, setCurrent] = useState(0)
+  const length = mentorsArray.length;
+  const nextImage = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1)
   }
+
+  const previousImage = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1)
+  }
+
+  if (!Array.isArray(mentorsArray) || mentorsArray.length <= 0) {
+    return null;
+  }
+
+  return (
+    <body className="chatpage">
+    <Stack direction="column" justifyContent="center" alignItems="center"
+    sx={{marginBottom: '-10px', marginTop: '10px'}}>
+    
+    <img
+        className="second-mapbox-quote"
+        src="https://cdn-images-1.medium.com/max/1600/1*GoiGM1iQZzdB5LbCR1u7eA.png" />
+
+       <section className="slider2">
+  
+        <img
+          className='mentorcard-left-arrow'
+          src="https://cdn-images-1.medium.com/max/800/1*UC5JK7wWPzUW6DFov8WrEA.png"
+          onClick={previousImage}
+        />
+
+        {mentorsArray.map((mentor, index) => {
+
+          return (
+            <div className={index === current ? 'mentor active' : 'mentor'} key={index}>
+              {index === current && (<SecondMapBox
+                key={mentor.id}
+                mentor={mentor}
+              />)}
+
+            </div>
+          )
+        })}
+        < img
+          className="mentorcard-right-arrow"
+          src="https://cdn-images-1.medium.com/max/800/1*YQEIDVTv2oWP2d_USO-P9Q.png"
+          onClick={nextImage} />
+    </section>
+    </Stack>
+    </body>
+  );
+
+}
 
 export default MapPage; 
